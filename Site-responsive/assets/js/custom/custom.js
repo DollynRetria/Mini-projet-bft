155773bab0ef2b0180a87ef3f8ac144ac37afca7
dollyn <retriadollyn@gmail.com>
2020-08-11 17:10:08 +0300

    version 1.2 [responsive]

--- Site-responsive/assets/js/custom/custom.js
+++ Site-responsive/assets/js/custom/custom.js
@@ -2,34 +2,53 @@ jQuery.noConflict();
 (function ($) {
     $(function () {
 		// code jquery ready
-      //   $('.slider-wrapper').slick({
-      //     dots : false,
-      //     arrows : false,
-      //     slidesToShow: 1,
-      //     autoplay:true,
-      //     variableWidth: true ,
-      //     slidesToScroll: 1,
-      //     speed:500,
-      //     centerPadding: '15px',
-      // });
-      //alert("ato");
-
-      $(".menu-pple > li").hover(function(){
-        if($(this).children('.submenu').length > 0){
-          $(this).children('.submenu').toggle();
-        }
-        //alert("ato");
-      });
+      var dist = ($(window).width() > 767) ? 130 : 115;
+      console.log(dist);
 
       $('.burger-menu').on("click", function(){
         $('body').toggleClass('pos-fixed');
         $(this).toggleClass('change');
         $('.header-menu').toggleClass('menu-show');
       })
+
+
+      $('#contact-me').on("click", function(e){
+        e.preventDefault();
+        var page = $(this).attr('href'); 
+        var speed = 1000; 
+        $('html, body').animate({ scrollTop: $(page).offset().top - dist}, speed ); 
+        return false;
+      });
+
     });
 
     $(window).on('load resize orientationchange', function () {
-		//code jquery onload, resize, orientationchange
+      //code jquery onload, resize, orientationchange
+      var dist = ($(window).width() > 767) ? 130 : 115;
+      if($(window).width() > 767){
+        $(".menu-pple > li").hover(function(){
+          if($(this).children('.submenu').length > 0){
+            $(this).children('.submenu').toggle();
+          }
+        });
+        $('.menu-pple > li > a[href^="#"]').on("click", function(e){
+          e.preventDefault();
+          var page = $(this).attr('href'); 
+          var speed = 1000; 
+          //$('.burger-menu').trigger('click');
+          $('html, body').animate({ scrollTop: $(page).offset().top - dist}, speed ); 
+          return false;
+        });
+      }else{
+        $('.menu-pple > li > a[href^="#"]').on("click", function(e){
+          e.preventDefault();
+          var page = $(this).attr('href'); 
+          var speed = 1000; 
+          $('.burger-menu').trigger('click');
+          $('html, body').animate({ scrollTop: $(page).offset().top - dist}, speed ); 
+          return false;
+        });
+      }
     });
 })(jQuery);
 
